8af5debf67608826abe42c140ae4e5fae1021b69
kir_snegir <ssnegurkirill@gmail.com>
2019-12-13 18:53:02 +0300

    Scripted auto-commit on change (2019-12-13 18:53:02) by gitwatch.sh

--- src/main/java/RaceData.java
+++ src/main/java/RaceData.java
@@ -1,19 +1,22 @@
 import java.io.Serializable;
 
 public class RaceData implements Serializable {
-    private float timeDelay, cancelValue, failsPersent;
-    private int counterTotal, counterFails;
+    private float timeDelay, failValue, failsPersent;
+    private int counterTotal, counterFail;
 
-    RaceData(String timeDelay, float cancelValue){
+    RaceData(String timeDelay, float failValue){
         if (timeDelay != ""){
             this.timeDelay = Float.parseFloat(timeDelay);
         } else this.timeDelay = 0;
-        this.cancelValue = cancelValue;
+        this.failValue = failValue;
+        this.counterTotal = 1;
+        this.counterFail = (this.timeDelay >= 1.0 || this.failValue == 1.0) ? 1 : 0;
+
     }
 
     RaceData(float timeDelay, int counterTotal, int counterFail){
         this.timeDelay = timeDelay;
         this.counterTotal = counterTotal;
-        this.counterFails = counterFail;
+        this.counterFail = counterFail;
     }
 }
