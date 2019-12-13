77c6280086f554ed0b4f1f8c404d7a0e096f0766
kir_snegir <ssnegurkirill@gmail.com>
2019-12-13 19:35:01 +0300

    Scripted auto-commit on change (2019-12-13 19:35:01) by gitwatch.sh

--- src/main/java/AirplanesSparkApp.java
+++ src/main/java/AirplanesSparkApp.java
@@ -21,7 +21,7 @@ public class AirplanesSparkApp {
         JavaRDD<String[]> flightsSplit  = flights.map(s -> Arrays.stream(s.split(" ")).toArray(String[]::new ));
         JavaRDD<String[]> airportsSplit  = airports.map(s -> Arrays.stream(s.split(" ")).toArray(String[]::new));
 
-        JavaPairRDD<String,String> flightsPair = flightsSplit.mapToPair(s -> new Tuple2<>(s[0],s[1]));
+//        JavaPairRDD<String,String> flightsPair = flightsSplit.mapToPair(s -> new Tuple2<>(s[0],s[1]));
         JavaPairRDD<String,String> airportsPair = airportsSplit.mapToPair(s ->{
             String[] airportAttr = CSVParser.parseAirportString(s.toString());
             return new Tuple2<String, String>(CSVParser.replaceQuotes(airportAttr[0]),CSVParser.replaceQuotes(airportAttr[1]));
