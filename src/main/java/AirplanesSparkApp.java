ee8b1d28c4c979983326ce3cf48637dbb49f93aa
kir_snegir <ssnegurkirill@gmail.com>
2019-12-13 19:35:27 +0300

    Scripted auto-commit on change (2019-12-13 19:35:27) by gitwatch.sh

--- src/main/java/AirplanesSparkApp.java
+++ src/main/java/AirplanesSparkApp.java
@@ -21,14 +21,16 @@ public class AirplanesSparkApp {
         JavaRDD<String[]> flightsSplit  = flights.map(s -> Arrays.stream(s.split(" ")).toArray(String[]::new ));
         JavaRDD<String[]> airportsSplit  = airports.map(s -> Arrays.stream(s.split(" ")).toArray(String[]::new));
 
-//        JavaPairRDD<String,String> flightsPair = flightsSplit.mapToPair(s -> new Tuple2<>(s[0],s[1]));
+
         JavaPairRDD<String,String> airportsPair = airportsSplit.mapToPair(s ->{
             String[] airportAttr = CSVParser.parseAirportString(s.toString());
             return new Tuple2<String, String>(CSVParser.replaceQuotes(airportAttr[0]),CSVParser.replaceQuotes(airportAttr[1]));
         });
-
         final Broadcast<Map<String, String>> airportsBroadcasted = sc.broadcast(airportsPair.collectAsMap());
 
+        JavaPairRDD<String,String> flightsPair = flightsSplit.mapToPair(s -> new Tuple2<>(s[0],s[1]));
+
+        
 //        JavaPairRDD<Tuple2<String,String>, RaceData> flightsReduce = flightsPair.reduceByKey();
 //        JavaPairRDD<Tuple2<String,String>, RaceData> airportReduce = airportsPair.reduceByKey();
     }
