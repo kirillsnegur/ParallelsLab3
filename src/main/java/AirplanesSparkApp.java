4b8275b64e6d0595a836a0fb12b4e106fa201a5d
kir_snegir <ssnegurkirill@gmail.com>
2019-12-13 19:33:42 +0300

    Scripted auto-commit on change (2019-12-13 19:33:42) by gitwatch.sh

--- src/main/java/AirplanesSparkApp.java
+++ src/main/java/AirplanesSparkApp.java
@@ -25,6 +25,8 @@ public class AirplanesSparkApp {
             return new Tuple2<String, String>(CSVParser.replaceQuotes(airportAttr[0]),CSVParser.replaceQuotes(airportAttr[1]));
         });
 
+        final Broadcast<Map<String, AirportData>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
+
 //        JavaPairRDD<Tuple2<String,String>, RaceData> flightsReduce = flightsPair.reduceByKey();
 //        JavaPairRDD<Tuple2<String,String>, RaceData> airportReduce = airportsPair.reduceByKey();
     }
