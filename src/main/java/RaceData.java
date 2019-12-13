fc96a7d9913249a22edb7407f7303121ddf3e8bb
kir_snegir <ssnegurkirill@gmail.com>
2019-12-14 00:03:44 +0300

    Scripted auto-commit on change (2019-12-14 00:03:44) by gitwatch.sh

--- src/main/java/RaceData.java
+++ src/main/java/RaceData.java
@@ -18,7 +18,7 @@ public class RaceData implements Serializable {
         this.timeDelay = timeDelay;
         this.counterTotal = counterTotal;
         this.counterFail = counterFail;
-        this.failsPersent = (this.counterTotal != 0) ? (float) counterFail * 100/counterTotal : 0;
+        this.failsPersent = counterFail * 100/counterTotal;
     }
 
     public float getTimeDelay() {return timeDelay;}
