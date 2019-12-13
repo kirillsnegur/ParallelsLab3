4b076a44d4c248465ac9fc305bd864d6044404d6
kir_snegir <ssnegurkirill@gmail.com>
2019-12-13 19:10:04 +0300

    Scripted auto-commit on change (2019-12-13 19:10:04) by gitwatch.sh

--- src/main/java/CSVParser.java
+++ src/main/java/CSVParser.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import org.apache.hadoop.io.Text;
 
 public class CSVParser {
+
+    public static String[] parseString(Text str){
+        return str.toString().split(",");
+    };
+
+    public static String[] parseStringQuote(Text str){
+        return str.toString().split(",(?=\")");
+    };
+
+
+    public static boolean isArrDelay(String v){
+        return  (v.equals("\"ARR_DELAY\""));
+    }
+
+//    public static boolean isDestAirport(String v){
+//        return  (v.equals("\"DEST_AIRPORT_ID\""));
+//    }
+
+    public static boolean isDesription(String v){
+        return  (v.equals("Code,Description"));
+    }
+
+    public static int getCell(String str){
+        return Integer.parseInt(str.replace("\"",""));
+    }
 }
